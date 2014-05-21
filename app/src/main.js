48b231218c6655bcf1f00edc205255e3c98591bf
artfoundry <dave@artfoundry.com>
2014-05-21 15:24:28 -0700

    bullets and collision added; this version is just a working branch

--- app/src/main.js
+++ app/src/main.js
@@ -9,7 +9,10 @@ define(function(require, exports, module) {
     var StateModifier = require('famous/modifiers/StateModifier');
     var Transitionable = require('famous/transitions/Transitionable');
     var View = require('famous/core/View');
-
+    var Particle = require('famous/physics/bodies/Particle');
+    var Circle = require('famous/physics/bodies/Circle');
+    var Collision = require('famous/physics/constraints/Collision');
+    var PhysicsEngine = require('famous/physics/PhysicsEngine');
 
     // create the main context
     var mainContext = Engine.createContext();
@@ -28,7 +31,11 @@ define(function(require, exports, module) {
                 tankView.translation.get());
         }
     });
-
+    var tankBoundary = new Circle({
+        radius: 50,
+        mass : Infinity,
+        position: [0,0]
+    });
 
     var node = tankView.add(tankModifier);
 
@@ -41,24 +48,21 @@ define(function(require, exports, module) {
     node.add(s1);
     s1.pipe(tankView);
 
+
     var s2 = new Surface({
         size: [50, 50],
         properties: {
             backgroundColor: 'red'
         }
     });
+
     mainContext.add(centerModifier).add(tankView);
     var gunPos = new StateModifier({
         transform: Transform.translate(50, 0, 0)
     });
-    node.add(gunPos).add(s2);
+    node.add(gunPos).add(s2).add(tankBoundary);
     s2.pipe(tankView);
 
-
-    var pos = 0;
-    var rot = 0;
-
-// alternatively, myView.subscribe(surface);
 
 // normally inside view module's code
     tankView._eventInput.on('click', function() {
@@ -95,9 +99,74 @@ define(function(require, exports, module) {
             if(angle > (Math.PI * 2)) angle = 0;
             else if(angle < 0) angle = (Math.PI * 2);
             tankView.rotation.set(angle);
-        }
-        else {
+        } else if (e.keyCode == 32) {
+
+        } else {
             console.log(e.keyCode);
         };
     });
+
+    var physicsEngine = new PhysicsEngine();
+
+    var bulletMotion = new Circle({
+        radius : 1,
+        position: [window.innerWidth/2 - 100,window.innerHeight/2 - 100,0],
+        velocity: [.5,.5,0]
+    });
+
+    var bulletSurface = new Surface({
+        size: [10,10],
+        properties : {
+            backgroundColor: 'red',
+            borderRadius: '10px'
+        }
+    });
+
+    physicsEngine.addBody(bulletMotion);
+    mainContext.add(bulletMotion).add(bulletSurface);
+
+
+    var blocksPos = [
+        [0.1,0.1],
+        [0.1,0.5],
+        [0.1,0.9],
+        [0.5,0.1],
+        [0.5,0.5],
+        [0.5,0.9],
+        [0.9,0.1],
+        [0.9,0.5],
+        [0.9,0.9],
+    ];
+
+    var blockBoundaries = [];
+
+    for (var i = 0; i < blocksPos.length; i++) {
+        var blockPos = blocksPos[i];
+        var blockBoundary = new Circle({
+            radius: 25,
+            mass : Infinity,
+            position: [blockPos[0] * window.innerWidth, blockPos[1] * window.innerHeight]
+        });
+
+        blockBoundaries.push(blockBoundary);
+
+        physicsEngine.addBody(blockBoundary);
+
+        var block = new Surface({
+            size: [50, 50],
+            properties: {
+                backgroundColor: 'black',
+                borderRadius: '5px'
+            }
+        });
+        var position = new StateModifier({
+            origin: blocksPos[i]
+        });
+        blockBoundaries.push(tankBoundary);
+        mainContext.add(blockBoundary).add(block);
+    }
+
+    var collision = new Collision({restitution :.3});
+    physicsEngine.attach(collision, blockBoundaries, bulletMotion);
+
 });
